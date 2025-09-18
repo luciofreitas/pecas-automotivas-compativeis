0fa43136288f534128d9c0e154f80706ac369e5c
luciofreitas <luciodfp@gmail.com>
2025-09-18 12:15:45 -0300

    Corrigir import CSS em CompatibilityModal.jsx (app.css -> App.css)

--- src/CompatibilityModal.jsx
+++ src/CompatibilityModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './app.css';
+import './App.css';
 
 export default function CompatibilityModal({ show, onClose, title, titleIcon, children }) {
 	useEffect(() => {
